6455e34ab929220da831854fef338d76733b5d5f
Luiz-Eduardo1985 <luiz.e.rodrigues.1985@gmail.com>
2024-03-29 21:35:08 -0300

    correção do arquivo de DB

--- Projeto_python_1_2/app.py
+++ Projeto_python_1_2/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, sqlite3
+import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
+
 
 app = Flask(__name__, template_folder='templates')
 
