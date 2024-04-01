2c94c5a5e03c36be81d041e9bcd0103383726c2f
Luiz-Eduardo1985 <luiz.e.rodrigues.1985@gmail.com>
2024-04-01 16:07:37 -0300

    bug no app

--- Projeto_python_1_2/app.py
+++ Projeto_python_1_2/app.py
@@ -4,11 +4,9 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__, template_folder='templates')
 
-conexao = sqlite3.connect('cadastros.db')
+#conexao = sqlite3.connect('cadastros.db')
 
 #criando variaveis para inserir no bando do dados
-nome = "luiz eduardo"
-cpf = "333.333.333.33"
 
 #criando cursos para inserir os dados
 # cursor = conexao.cursor()
@@ -32,13 +30,20 @@ cpf = "333.333.333.33"
 
 
 # criando cursor para selecionar todo o banco
-# cursor = conexao.cursor()
-# sql_select_full = "SELECT * FROM tabela_cadastro"
-
-# cursor.execute(sql_select_full)
-# lista_cadastro = cursor.fetchall()
-# for item in lista_cadastro:
-#     print(item)
+def listar_pessoas():
+    try:
+        conexao = sqlite3.connect('cadastros.db')
+        cursor = conexao.cursor()
+        sql_select_full = "SELECT * FROM tabela_cadastro"
+        cursor.execute(sql_select_full)
+        pessoas = cursor.fetchall()
+        conexao.close()
+        return pessoas
+    except :
+        return False
+    
+    
+    
 
 
 # criando cursor para atualizar um dado especifico atraves do ID
@@ -49,56 +54,105 @@ cpf = "333.333.333.33"
 
 
 # criando cursor para deletar um dado especifico atraves do ID
-cursor = conexao.cursor()
-sql_delete = "DELETE FROM tabela_cadastro WHERE id_pessoa = ? "
-cursor.execute(sql_delete, (2, ))
-conexao.commit()
+# cursor = conexao.cursor()
+# sql_delete = "DELETE FROM tabela_cadastro WHERE id_pessoa = ? "
+# cursor.execute(sql_delete, (2, ))
+# conexao.commit()
 
 
-
-
-
-
+#gerar id ou recuperar ultimo id usado
+def gerar_id():
+    conexao = sqlite3.connect('cadastros.db')
+    cursor = conexao.cursor()
+    cursor.execute("select seq from sqlite_sequence where name = 'tabela_cadastro'")
+    next_id = cursor.fetchone()[0]
+    return next_id + 1
 
 
 # Lista para armazenar os dados das pessoas cadastradas
-# TESTE 3 qual arquivo usar
+
 
 pessoas = []
 
 # Rota para exibir a página inicial com a lista de pessoas cadastradas e opções CRUD
 @app.route('/')
 def index():
-    return render_template('index.html', pessoas=pessoas)
+    lista_pessoas = cadastros.listar_pessoas()
+    return render_template('index.html', pessoas=lista_pessoas)
 
 # Rota para cadastrar uma nova pessoac
 @app.route('/cadastrar', methods=['POST'])
-def cadastrar_pessoa():
-    nome = request.form['nome']
-    cpf = request.form['cpf']
-    pessoas.append({'nome': nome, 'cpf': cpf})
-    return redirect(url_for('index'))
 
-# Rota para editar uma pessoa
-@app.route('/editar/<cpf>', methods=['GET', 'POST'])
-def editar_pessoa(cpf):
-    for pessoa in pessoas:
-        if pessoa['cpf'] == cpf:
-            if request.method == 'POST':
-                pessoa['nome'] = request.form['nome']
-                pessoa['cpf'] = request.form['cpf']
-                return redirect(url_for('index'))
-            return render_template('editar.html', pessoa=pessoa)
-    return "Pessoa não encontrada."
+#função de cadastrar pessoas
+def cadastrar_pessoa():
+    try:
+        conexao = sqlite3.connect('cadastros.db')
+        cursor = conexao.cursor()
+        sql_insert = "insert into tabela_cadastro (nome, cpf) values (?, ?)"
+        cursor.execute(sql_insert, (nome, cpf))
+        id_pessoa = cursor.lastrowid
+        conexao.commit()
+        conexao.close()
+        return id_pessoa
+    except Exception as ex:
+        print(ex)
+        return False
+    
+    # nome = request.form['nome']
+    # cpf = request.form['cpf']
+    # pessoas.append({'nome': nome, 'cpf': cpf})
+    # return redirect(url_for('index'))
+
+
+
+
+
+# Rota para editar/atualizar uma pessoa
+@app.route('/editar/<id_pessoa>', methods=['GET', 'POST'])
+
+def editar_pessoa(id_pessoa:int, nome, cpf):
+    try:
+        conexao = sqlite3.connect('cadastros.db')
+        cursor = conexao.cursor()
+        sql_update = "UPDATE tabela_cadastro SET NOME = ? WHERE id_pessoa = ?"
+        cursor.execute(sql_update, (nome, cpf))
+        conexao.commit()
+        conexao.close()
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
+        
+        # for pessoa in pessoas:
+        #     if pessoa['id_pessoa'] == id_pessoa:
+        #         if request.method == 'POST':
+        #             pessoa['nome'] = request.form['nome']
+        #             pessoa['cpf'] = request.form['cpf']
+        #             return redirect(url_for('index'))
+        #         return render_template('editar.html', pessoa=pessoa)
+        # return "Pessoa não encontrada."
 
 # Rota para excluir uma pessoa
-@app.route('/excluir/<cpf>')
-def excluir_pessoa(cpf):
-    for pessoa in pessoas:
-        if pessoa['cpf'] == cpf:
-            pessoas.remove(pessoa)
-            return redirect(url_for('index'))
-    return "Pessoa não encontrada."
+@app.route('/excluir/<id_pessoa>')
+def excluir_pessoa(id_pessoa:int):
+    try:
+        conexao = sqlite3.connect('cadastros.db')
+        cursor = conexao.cursor()
+        sql_delete = "DELETE FROM tabela_cadastro WHERE id_pessoa = ? "
+        cursor.execute(sql_delete, (id_pessoa, ))
+        conexao.commit()
+        conexao.close()
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
+
+
+    # for pessoa in pessoas:
+    #     if pessoa['cpf'] == cpf:
+    #         pessoas.remove(pessoa)
+    #         return redirect(url_for('index'))
+    # return "Pessoa não encontrada."
 
 if __name__ == '__main__':
     app.run(debug=True)
