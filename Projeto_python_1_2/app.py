0cc21a3b75b8bd546c6d9155d13536824816c639
Luiz-Eduardo1985 <luiz.e.rodrigues.1985@gmail.com>
2024-04-01 13:30:35 -0300

    implementação dos cruds

--- Projeto_python_1_2/app.py
+++ Projeto_python_1_2/app.py
@@ -5,6 +5,62 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__, template_folder='templates')
 
 conexao = sqlite3.connect('cadastros.db')
+
+#criando variaveis para inserir no bando do dados
+nome = "luiz eduardo"
+cpf = "333.333.333.33"
+
+#criando cursos para inserir os dados
+# cursor = conexao.cursor()
+# sql_insert = "insert into tabela_cadastro (nome, cpf) values (?, ?)"
+
+# cursor.execute(sql_insert, (nome, cpf))
+
+# id_pessoa = cursor.lastrowid
+# conexao.commit()
+# print(f"O ultimo codigo inserido foi {id_pessoa}")
+
+
+#criando cursor para selecionar dados do banco
+# print("pessoa  id 2 ")
+# cursor = conexao.cursor()
+# sql_select_unico = "SELECT * FROM tabela_cadastro WHERE  id_pessoa = ?"
+
+# cursor.execute(sql_select_unico, (2, ))
+# id_pessoa, nome, cpf = cursor.fetchone()
+# print(f"{id_pessoa}----------{nome}-----------{cpf}")
+
+
+# criando cursor para selecionar todo o banco
+# cursor = conexao.cursor()
+# sql_select_full = "SELECT * FROM tabela_cadastro"
+
+# cursor.execute(sql_select_full)
+# lista_cadastro = cursor.fetchall()
+# for item in lista_cadastro:
+#     print(item)
+
+
+# criando cursor para atualizar um dado especifico atraves do ID
+# cursor = conexao.cursor()
+# sql_update = "UPDATE tabela_cadastro SET NOME = ? WHERE id_pessoa = ?"
+# cursor.execute(sql_update, ("Virginia aparecida", 2))
+# conexao.commit()
+
+
+# criando cursor para deletar um dado especifico atraves do ID
+cursor = conexao.cursor()
+sql_delete = "DELETE FROM tabela_cadastro WHERE id_pessoa = ? "
+cursor.execute(sql_delete, (2, ))
+conexao.commit()
+
+
+
+
+
+
+
+
 # Lista para armazenar os dados das pessoas cadastradas
 # TESTE 3 qual arquivo usar
 
@@ -15,7 +71,7 @@ pessoas = []
 def index():
     return render_template('index.html', pessoas=pessoas)
 
-# Rota para cadastrar uma nova pessoa
+# Rota para cadastrar uma nova pessoac
 @app.route('/cadastrar', methods=['POST'])
 def cadastrar_pessoa():
     nome = request.form['nome']
